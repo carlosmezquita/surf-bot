f1df6835cd87afdc0690488c0e9fbce731ef3e31
Carlos Mezquita <mezquitalobato@gmail.com>
2022-11-03 19:34:32 +0100

    tweet publisher added

---
+++ twitterbot/tweet.js
@@ -0,0 +1,27 @@
+const client = require(process.cwd() + '/twitterbot/twitterClient.js')
+const fs = require('fs');
+const { Module } = require('module');
+const card = require(process.cwd() + '/utils/forecast')
+
+async function forecast(spot) {
+    try {
+        const contents = fs.readFileSync(process.cwd() + "/data/spots.json");
+        // // Define to JSON type
+        const spotsData = JSON.parse(contents);
+
+        let today = new Date();
+        let date = today.getDate() + "/" + (today.getMonth() + 1) + "/" + today.getFullYear();
+
+        await card.cardGenerator(spot)
+        const picID = await client.v1.uploadMedia(process.cwd() + `/data/spots/${spot}Card.jpeg`);
+
+        // mediaIds is a string[], can be given to .tweet
+        await client.v1.tweet(`📍 ${spotsData[spot].name}\n🗓️ ${date}`, { media_ids: picID });
+
+        console.log("Tweet published successfully!");
+    } catch (e) {
+        console.error(e);
+    }
+}
+module.exports = { forecast };
+// forecast("orzan");
