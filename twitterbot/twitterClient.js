9cb094c77d291248bf1c75f55fbe3d81b3b9791a
carlos.mezquita <mezquitalobato@gmail.com>
2022-11-26 01:02:20 +0100

    env keys

--- twitterbot/twitterClient.js
+++ twitterbot/twitterClient.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 const { TwitterApi } = require("twitter-api-v2");
 
 const client = new TwitterApi({
-    appKey: TW_APP_KEY,
-    appSecret: TW_APP_SECRET,
-    accessToken: TW_ACCESS_TOKEN,
-    accessSecret: TW_ACCESS_SECRET
+    appKey: process.env.TW_APP_KEY,
+    appSecret: process.env.TW_APP_SECRET,
+    accessToken: process.env.TW_ACCESS_TOKEN,
+    accessSecret: process.env.TW_ACCESS_SECRET
 })
 
 const rwClient = client.readWrite
